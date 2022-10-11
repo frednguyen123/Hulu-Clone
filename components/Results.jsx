3335243efeb6ce3e8573a1008c6a44648ac4bb8c
frednguyen123 <fred.nguyen.2000@gmail.com>
2022-10-10 23:19:26 -0700

    Completed Nav Component + Server Side Rendering

---
+++ components/Results.jsx
@@ -0,0 +1,11 @@
+const Results = ({ results }) => {
+    return (
+        <div>
+            {results.map(result => {
+                <Thumbnail key={result.id} result={result}/>
+            })}
+        </div>
+    );
+}
+
+export default Results;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Results from '../components/Results';
+import styles from '../styles/Home.module.css';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -16,7 +18,22 @@ export default function Home() {
       
       <Nav />
 
-      {/* Results */}
+      <Results results={results}/>
+
     </div>
   )
+}
+
+// server render happens first
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
+    ).then((res) => res.json());
+
+  return{
+    props:{
+      results: request.results,
+    },  
+  }
 }
